1a32d0a43967c1932819947d2a5477976eaa31d0
Walter Wang <csw.dawm@gmail.com>
2016-06-14 23:01:30 +0800

    Add clj-http

--- src/my_messenger_bot_app_clojure/core.clj
+++ src/my_messenger_bot_app_clojure/core.clj
@@ -5,15 +5,17 @@
             [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
             [org.httpkit.client :as http]
-            [my-messenger-bot-app-clojure.message :as message]))
+            [clj-http.client :as client]
+            [my-messenger-bot-app-clojure.message :as message])
+  (:use org.httpkit.server))
 
+(def listening-port 3000)
 (def config (load-file "config.clj"))
 (def access-token (config :access-token))
 (def verify-token (config :verify-token2))
 (def messenger-url "https://graph.facebook.com/v2.6/me/messages")
 (def headers {"Content-Type" "application/json"})
-(def options {:timeout 1000 :headers headers})
-
+;(def options {:timeout 100 :headers headers :keepalive 1000}) ;; for http-kit
 (defn check-webhook [query-string]
   (if (= (query-string :hub.verify_token) verify-token)
     {:status 200
@@ -33,17 +35,21 @@
                         first
                         :messaging
                         first)]
-    (let [sender (get-in messaging [:sender :id])
-          message (get-in messaging [:message :text])
+    (let [sender (-> messaging :sender :id)
+          message (-> messaging :message :text)
           timestamp (messaging :timestamp)
-          send-msg (message/create-text-message sender message)
-          resp (http/post (str messenger-url "?access_token=" access-token)
-                          (assoc options :body (json/write-str send-msg)))]
-      (:body @resp))))
+          send-msg (message/create-text-message sender message)]
+      ;resp (http/post (str messenger-url "?access_token=" access-token)
+                      ;(assoc options :body (json/write-str send-msg)))
+      ;(:body @resp)
+      (client/post (str messenger-url "?access_token=" access-token)
+                   {:body (json/write-str send-msg)
+                    :content-type :json}))))
 
 (defroutes main-routes
   (GET "/webhook" req (webhook-get-handler req))
   (POST "/webhook" req (webhook-post-handler req)))
 
 (defn -main []
-  (jetty/run-jetty main-routes {:port 3000}))
+  (jetty/run-jetty main-routes {:port listening-port}))
+  ;(run-server main-routes {:port listening-port}))
